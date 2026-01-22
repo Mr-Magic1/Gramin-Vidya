e2e50dbcba40a5a205bbf7bad7f3b0d7d312fcf0
kailash vishwakarma <vishwakarmakailash123@gmail.com>
2026-01-22 21:29:49 +0530

    solving error

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,12 +75,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import dj_database_url
+import os
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
