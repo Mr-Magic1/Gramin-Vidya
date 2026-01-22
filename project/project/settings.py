75ac682b4c0b81dcf945db218e53a7d7d2291113
PraVigil_01 <amandeepprakash6@gmail.com>
2026-01-22 22:41:06 +0530

    database recovered

--- project/project/settings.py
+++ project/project/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 import dj_database_url
 import os
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
